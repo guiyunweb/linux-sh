8d3c34377d12ec42f4f7f451552aa0308646e655
孟归云 <ryougicloud@gmail.com>
2022-10-27 03:47:04 +0000

    更新 goods_nginx.sh

--- goods_nginx.sh
+++ goods_nginx.sh
@@ -14,7 +14,7 @@ git clone -b dev https://github.com/cuber/ngx_http_google_filter_module
 tar -zxvf tengine-2.3.3.tar.gz
 tar -zxvf openssl-1.1.1k.tar.gz 
 tar -zxvf pcre-8.45.tar.gz
-tar -zxvf zlib-1.2.12.tar.gz
+tar -zxvf zlib-1.2.13.tar.gz
 
 cd openssl-1.1.1k/
 ./config && make && make install
@@ -25,7 +25,7 @@ cd tengine-2.3.3/
   --prefix=/usr/local/nginx \
   --with-pcre=../pcre-8.45 \
   --with-openssl=../openssl-1.1.1k \
-  --with-zlib=../zlib-1.2.12 \
+  --with-zlib=../zlib-1.2.13 \
   --add-module=../ngx_http_google_filter_module \
   --add-module=../ngx_http_substitutions_filter_module \
   --with-file-aio --with-stream --with-http_auth_request_module --with-http_ssl_module --with-http_v2_module --with-http_realip_module --with-http_addition_module --with-http_xslt_module=dynamic --with-http_image_filter_module=dynamic --with-http_geoip_module=dynamic --with-http_sub_module --with-http_dav_module --with-http_flv_module --with-http_mp4_module --with-http_gunzip_module --with-http_gzip_static_module --with-http_random_index_module --with-http_secure_link_module --with-http_degradation_module --with-http_slice_module --with-http_stub_status_module --with-http_perl_module=dynamic --with-mail=dynamic --with-mail_ssl_module --with-pcre --with-pcre-jit --with-stream=dynamic --with-stream_ssl_module --with-google_perftools_module --with-debug
